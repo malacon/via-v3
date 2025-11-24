4211353dc47ab3fab764bd5a3c02df63d1139d49
Craig Baker <craig.d.baker+github@gmail.com>
2025-11-23 23:24:37 -0600

    picture

--- app/routes/healthz.ts
+++ app/routes/healthz.ts
@@ -4,18 +4,18 @@ import { prisma } from '#app/utils/db.server.ts'
 export async function loader(_args: LoaderFunctionArgs) {
 	let db: 'ok' | 'timeout-or-error' | 'skipped' = 'skipped'
 
-	const ping = (async () => {
-		try {
-			await prisma.$queryRaw`SELECT 1`
-			db = 'ok'
-		} catch {
-			db = 'timeout-or-error'
-		}
-	})()
+	// const ping = (async () => {
+	// 	try {
+	// 		await prisma.$queryRaw`SELECT 1`
+	// 		db = 'ok'
+	// 	} catch {
+	// 		db = 'timeout-or-error'
+	// 	}
+	// })()
 
-	const softTimeout = new Promise<void>((resolve) => setTimeout(resolve, 300))
+	// const softTimeout = new Promise<void>((resolve) => setTimeout(resolve, 300))
 
-	await Promise.race([ping, softTimeout])
+	// await Promise.race([ping, softTimeout])
 
 	return data({ status: 'ok', db })
 }
