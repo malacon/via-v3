9d22b4fbf7d3c82dc35b3b58ce6054c603df229d
Craig Baker <craig.d.baker+github@gmail.com>
2026-03-08 21:37:48 -0500

    feat: qrcode

--- app/routes/qrcode.ts
+++ app/routes/qrcode.ts
@@ -1,7 +1,7 @@
 import { type LoaderFunctionArgs } from 'react-router'
 import * as QRCode from 'qrcode'
 
-const SITE_URL = 'https://via.studyworkpray.org/'
+const SITE_URL = 'https://via.studyworkpray.org/contact'
 
 export async function loader(_args: LoaderFunctionArgs) {
 	const buffer = await QRCode.toBuffer(SITE_URL)
